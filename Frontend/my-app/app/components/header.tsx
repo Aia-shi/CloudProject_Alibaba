3d7d61c09c145924fd39f61b75f3cab82e963bf6
Dulik-M <dulikmarcin03@gmail.com>
2025-11-13 13:06:55 +0100

    liitle header fix

--- Frontend/my-app/app/components/header.tsx
+++ Frontend/my-app/app/components/header.tsx
@@ -1,16 +1,17 @@
 export default function Header() {
   return (
-    <header className="w-full items-center bg-black">
+    <header className="w-full items-center">
       {/* make this a flex row so children can sit side-by-side */}
       <div className="p-5 flex items-center w-full">
-        <div className="w-1/2">
-          <p className="p-1 text-left text-white">Inteligentny Planner Wydatków</p>
+        <div className="w-1/2 flex items-center gap-3">
+          <img src="/logo planer.png" alt="Logo" className="h-15 w-auto" />
+          <p className="p text-xl text-left text-white"><b>Inteligentny Planner Wydatków</b></p>
         </div>
 
         <div className="w-1/2 flex justify-end items-center gap-3 ">
-          <p className="text-xs">Team</p>
-          <p className="text-xs">Technologie Cloud</p>
-          <p className="text-xs">Github</p>
+          <p className="">Team</p>
+          <p className="">Technologie Cloud</p>
+          <p className="">Github</p>
         </div>
       </div>
     </header>
